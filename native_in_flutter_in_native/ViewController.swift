3ac008f5daaa11f11d789ca2bde61b3b9c060f91
vincenzo.calia <vincenzo.calia@mmbsoftware.it>
2024-01-03 12:23:24 +0100

    Initial Commit

---
+++ native_in_flutter_in_native/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  native_in_flutter_in_native
+//
+//  Created by Vincenzo Calia on 03/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
