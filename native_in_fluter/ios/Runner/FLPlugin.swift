40c80c783670ae4bce54af937e924924cc66e820
vincenzo.calia <vincenzo.calia@mmbsoftware.it>
2024-01-04 14:32:06 +0100

    Little projects clean up

---
+++ native_in_fluter/ios/Runner/FLPlugin.swift
@@ -0,0 +1,8 @@
+//
+//  FLPlugin.swift
+//  Runner
+//
+//  Created by Vincenzo Calia on 04/01/24.
+//
+
+import Foundation
---
+++ native_in_flutter_in_native/FLPlugin.swift
@@ -0,0 +1,8 @@
+//
+//  FLPlugin.swift
+//  native_in_flutter_in_native
+//
+//  Created by Vincenzo Calia on 04/01/24.
+//
+
+import Foundation
